7ce28ea4264516509f5954510c36f451936604db
StevenWeng <stevenweng@systex.com.tw>
2015-08-10 16:48:22 +0800

    init

---
+++ App_Code/HelloWorld.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for HelloWorld
+/// </summary>
+public class HelloWorld
+{
+	public HelloWorld()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    public String sayHi()
+    {
+        return "Hi !!";
+    }
+}
