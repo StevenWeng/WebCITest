b25a0bba82fe490dffead0442ae0ecad87705121
StevenWeng <stevenweng@systex.com.tw>
2015-08-13 16:22:47 +0800

    更新 web.config 增加 Newtonsoft.Json 套件，並簡單測試

---
+++ App_Code/test/Json/JsonConvertTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NUnit.Framework;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+/// <summary>
+/// Summary description for JsonConvertTest
+/// </summary>
+[TestFixture]
+public class JsonConvertTest
+{
+    [Test]
+    public void testJObject() {
+        JObject jo = new JObject();
+        jo["msg"] = "Hello";
+        String joStr = jo.ToString();
+
+        JObject cloneJo = JsonConvert.DeserializeObject<JObject>(joStr);
+
+        Assert.AreEqual(jo, cloneJo);
+    }
+}
